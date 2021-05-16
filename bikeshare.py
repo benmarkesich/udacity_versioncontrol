26b72506e1a062de4e23a65f67cc7f63960e09d9
Ben Markesich <ben.markesich@gmail.com>
2021-05-17 06:02:49 +1000

    Changed months to abbreviations to improve efficency

--- bikeshare.py
+++ bikeshare.py
@@ -10,7 +10,8 @@ CITY_DATA = {'chicago': 'chicago.csv',
              'new york city': 'new_york_city.csv',
              'washington': 'washington.csv'}
 
-months = ('january', 'february', 'march', 'april', 'may', 'june')
+months = ('jan', 'feb', 'mar', 'apr', 'may', 'jun')
+"""months changed to improve efficiency"""
 
 weekdays = ('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday',
             'saturday')
